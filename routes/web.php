558609c8bff7811134c7e030cf6dad9d672e6c49
vodangkhoa2001 <vdk0610@gmail.com>
2022-05-25 17:18:18 +0700

    add folder web

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('home');
 })->name('home');
 Route::get('/users', function () {

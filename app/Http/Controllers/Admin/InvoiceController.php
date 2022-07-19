1292902a575f71564a372b1392d114a8d409e119
Saz48755 <dev_enterprise@ggroup.vn>
2022-07-20 00:28:51 +0700

    hoan_tien_zalopay

--- app/Http/Controllers/Admin/InvoiceController.php
+++ app/Http/Controllers/Admin/InvoiceController.php
@@ -250,6 +250,80 @@ class InvoiceController extends Controller
             // }
 
 
+            if($invoice->type_pay == 'ZaloPay'){
+
+                //Truy vấn trạng thái thanh toán của đơn hàng
+                $config = [
+                    "app_id" => 2554,
+                    "key1" => "sdngKKJmqEMzvh5QQcdD2A9XBSKUNaYn",
+                    "key2" => "trMrHtvjo6myautxDUiAcYsVtaeQ8nhf",
+                    "refund_url" => "https://sb-openapi.zalopay.vn/v2/query"
+                ];
+                
+                $app_trans_id = $invoice->invoice_code;  // Input your app_trans_id
+                $data = $config["app_id"]."|".$app_trans_id."|".$config["key1"]; // app_id|app_trans_id|key1
+                $params = [
+                "app_id" => $config["app_id"],
+                "app_trans_id" => $app_trans_id,
+                "mac" => hash_hmac("sha256", $data, $config["key1"])
+                ];
+                
+                $context = stream_context_create([
+                    "http" => [
+                        "header" => "Content-type: application/x-www-form-urlencoded\r\n",
+                        "method" => "POST",
+                        "content" => http_build_query($params)
+                    ]
+                ]);
+                
+                $resp = file_get_contents($config["refund_url"], false, $context);
+                $result = json_decode($resp, true);
+                $zp_trans_id = $result["zp_trans_id"];
+
+                //Hoàn tiền giao dịch
+                $config = [
+                    "app_id" => 2554,
+                    "key1" => "sdngKKJmqEMzvh5QQcdD2A9XBSKUNaYn",
+                    "key2" => "trMrHtvjo6myautxDUiAcYsVtaeQ8nhf",
+                    "refund_url" => "https://sb-openapi.zalopay.vn/v2/refund"
+                ];
+
+                $timestamp = round(microtime(true) * 1000); // miliseconds
+                $uid = "$timestamp".rand(111,999); // unique id 
+
+                $params = [
+                    "app_id" => $config["app_id"],
+                    "m_refund_id" => date("ymd")."_".$config["app_id"]."_".$uid,
+                    "timestamp" => $timestamp,
+                    "zp_trans_id" => $zp_trans_id,
+                    "amount" => $invoice->until_price,
+                    "description" => "ZaloPay Intergration Demo"
+                ];
+
+                // app_id|zp_trans_id|amount|description|timestamp
+                $data = $params["app_id"]."|".$params["zp_trans_id"]."|".$params["amount"]
+                ."|".$params["description"]."|".$params["timestamp"];
+                $params["mac"] = hash_hmac("sha256", $data, $config["key1"]);
+
+                $context = stream_context_create([
+                "http" => [
+                    "header" => "Content-type: application/x-www-form-urlencoded\r\n",
+                    "method" => "POST",
+                    "content" => http_build_query($params)
+                ]
+                ]);
+
+                $resp = file_get_contents($config["refund_url"], false, $context);
+                $result = json_decode($resp, true);
+
+                // foreach ($result as $key => $value) {
+                //     echo "$key: $value<br>";
+                // }
+
+                // dd($result);
+            }
+
+
             $user2 = User::select('users.*')
             ->join('invoices', 'invoices.user_id', '=', 'users.id')
             ->where('invoices.id', '=', $id)
